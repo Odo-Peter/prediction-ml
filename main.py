dafbdb825b99021a072502e9b9ee3e79652f6c78
Odo Peter Ebere <odopeter33@gmail.com>
2025-02-14 09:14:19 +0100

    fix: removed asynchronous pattern since most modules are synchronous

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import nest_asyncio
 import numpy as np
 from fastapi.middleware.cors import CORSMiddleware
 import pickle 
+import os
 
 
 # Initialize FastAPI app
@@ -24,9 +25,24 @@ app.add_middleware(
 # Run the server
 # nest_asyncio.apply()
 
+# Get absolute file paths for model & scaler
+model_path = os.path.abspath("./src/prediction_models/car_purchase_decision_model.pkl")
+scaler_path = os.path.abspath("./src/prediction_models/car_purchase_decision_scaler.pkl")
+
 # Load the saved model and scaler
-car_model = pickle.load(open('./src/prediction_models/car_purchase_decision_model.pkl', 'rb'))
-car_scaler = pickle.load(open('./src/prediction_models/car_purchase_decision_scaler.pkl', 'rb'))
+# car_model = pickle.load(open('./src/prediction_models/car_purchase_decision_model.pkl', 'rb'))
+# car_scaler = pickle.load(open('./src/prediction_models/car_purchase_decision_scaler.pkl', 'rb'))
+# Load the saved model and scaler
+try:
+    with open(model_path, "rb") as model_file:
+        car_model = pickle.load(model_file)
+
+    with open(scaler_path, "rb") as scaler_file:
+        car_scaler = pickle.load(scaler_file)
+
+    print("✅ Model and Scaler Loaded Successfully!")
+except Exception as e:
+    print(f"❌ Error Loading Model: {e}")
 
 
 # Define input schema
@@ -40,15 +56,15 @@ def read_root():
 
 # Define prediction endpoint
 @app.post("/predict/car_purchase")
-async def predict_car_purchase(input_data: CarPurchase):
+def predict_car_purchase(input_data: CarPurchase):
   # Convert input list to NumPy array
-  input_array = await np.array(input_data.features).reshape(1, -1)
+  input_array = np.array(input_data.features).reshape(1, -1)
 
   # scale the input features
-  scaled_data = await car_scaler.transform(input_array)
+  scaled_data = car_scaler.transform(input_array)
 
   # Make prediction
-  prediction = await car_model.predict(scaled_data)
+  prediction = car_model.predict(scaled_data)
   result = "Most likely to purchase a car" if prediction[0] == 1 else "Bad Wine"
 
   return {"prediction": result}
